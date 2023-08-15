85bd2774cb52ff649e30a054495293393564b68d
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-08-15 20:27:41 +0300

    HomeWork1

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -3,7 +3,44 @@
 
 int main()
 {
-    std::cout << "Abdulmajeed";
+    
+    std::cout << "**************************\n";
+    std::cout << "Name: Abdulmajeed Bakr.\n";
+    std::cout << "Age : 21 Years.\n";
+    std::cout << "City: Makkah.\n";
+    std::cout << "Country: Saudi Arabia.\n";
+    std::cout << "**************************\n\n\n";
+
+
+
+    std::cout << "********\n";
+    std::cout << "********\n";
+    std::cout << "********\n";
+    std::cout << "********\n\n\n";
+
+
+    std::cout << "I Love Programming!\n\n";
+    std::cout << "I promise to be the best develper ever!\n\n";
+    std::cout << "I know it will take time to practice, but I\nwill achieve my goal.\n\n";
+    std::cout << "Best Regards,\n";
+    std::cout << "Abdulmajeed Bakr.\n\n\n";
+
+
+    std::cout << "*       *\n";
+    std::cout << "*       *\n";
+    std::cout << "* * * * *\n";
+    std::cout << "*       *\n";
+    std::cout << "*       *\n";
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
