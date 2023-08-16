d920a7bfb374c227b3bcea6e74a41045b02bad91
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-08-16 17:42:55 +0300

    HomeWork: using namespace and Delete All "std::" using alt shortCut

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,36 +1,37 @@
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
     
-    std::cout << "**************************\n";
-    std::cout << "Name: Abdulmajeed Bakr.\n";
-    std::cout << "Age : 21 Years.\n";
-    std::cout << "City: Makkah.\n";
-    std::cout << "Country: Saudi Arabia.\n";
-    std::cout << "**************************\n\n\n";
+    cout << "**************************\n";
+    cout << "Name: Abdulmajeed Bakr.\n";
+    cout << "Age : 21 Years.\n";
+    cout << "City: Makkah.\n";
+    cout << "Country: Saudi Arabia.\n";
+    cout << "**************************\n\n\n";
 
 
 
-    std::cout << "********\n";
-    std::cout << "********\n";
-    std::cout << "********\n";
-    std::cout << "********\n\n\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n\n\n";
 
 
-    std::cout << "I Love Programming!\n\n";
-    std::cout << "I promise to be the best develper ever!\n\n";
-    std::cout << "I know it will take time to practice, but I\nwill achieve my goal.\n\n";
-    std::cout << "Best Regards,\n";
-    std::cout << "Abdulmajeed Bakr.\n\n\n";
+    cout << "I Love Programming!\n\n";
+    cout << "I promise to be the best develper ever!\n\n";
+    cout << "I know it will take time to practice, but I\nwill achieve my goal.\n\n";
+    cout << "Best Regards,\n";
+    cout << "Abdulmajeed Bakr.\n\n\n";
 
 
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "* * * * *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "* * * * *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
 
 
 
