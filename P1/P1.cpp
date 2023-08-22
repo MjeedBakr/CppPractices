1ac1f2708e3699e929319d4418a2b6597497e6b5
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-08-22 10:34:06 +0300

    Variables and constants Practice

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -3,17 +3,35 @@
 using namespace std;
 
 int main()
-{
-    
-    cout << "\a\n" << endl;
+{    
+    int myAge;
+    myAge = 20;
 
-    cout << "Dear Sir\\Madam, \n" << endl;
-    cout << "How are you?\n" << endl;
-    cout << "My name is \"Abdulmajeed\" ,nice to meet you.\n" << endl;
+    cout << "My age is " << myAge << " years old" << endl;
 
-    cout << "Ali\tAhmad\tLina\n";
-    cout << "Fadi\tZain\tMona\n";
+    int myNumber = 52;
+    float myFloat = 7.84;
+    double myDouble = 21.89822;
+    char myLetter = 'M \'';
+    string myString = "Mjeed";
+    bool myBoolean = true;
 
+    cout << myNumber << endl;
+    cout << myFloat << endl;
+    cout << myDouble << endl;
+    cout << myLetter << endl;
+    cout << myString << endl;
+    cout << myBoolean << endl;
+
+    int x = 4, y = 10, sum = x + y;
+    cout << sum << endl;
+
+    char char1 = 'A', char2 = 'B', char3 = 'C';
+    cout << char1 << char2 << char3 << " reversed is " << char3 << char2 << char1 << endl << endl;
+
+
+    const float PI = 3.14;
+    cout << PI << endl;
 
 
 
