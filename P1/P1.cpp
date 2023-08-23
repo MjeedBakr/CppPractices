1886223720a173297bec534c5643516a8883028d
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-08-23 17:40:48 +0300

    5# Homework: User Input

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main()
 {    
-    string name = "Mohammed Abu-Hadhud.";
-    int age = 44;
-    string city = "Amman.", country = "Jordan.";
-    int monthlySalary = 5000, yearlySalary = monthlySalary * 12;
-    char gender = 'M';
-    bool isMarried = true;
+    string name;
+    int age;
+    string city, country;
+    int monthlySalary;
+    char gender;
+    bool isMarried;
+
+    cout << "Enter your full name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your city name ";
+    cin >> city;
+    cout << "Enter your country name: ";
+    cin >> country;
+    cout << "Enter your monthly salary: ";
+    cin >> monthlySalary;
+    cout << "Enter your Gender \'M\' or \'F\'?";
+    cin >> gender;
+    cout << "Are you married \'true\' of \'false\'? ";
+    cin >> isMarried;
+    cout << endl;
 
     cout << "***********************************" << endl;
     cout << "Name: " << name << "\n";
@@ -17,7 +33,7 @@ int main()
     cout << "City: " << city << "\n";
     cout << "Country: " << country << "\n";
     cout << "Monthly Salary: " << monthlySalary << "\n";
-    cout << "Yearly Salary: " << yearlySalary << "\n";
+    cout << "Yearly Salary: " << monthlySalary * 12 << "\n";
     cout << "Gender: " << gender << "\n";
     cout << "Married: " << isMarried << "\n";
     cout << "***********************************" << endl << endl;
@@ -25,12 +41,22 @@ int main()
 
 
 
-    int firstNum = 20, secondNum = 30, thirdNum = 10;
-    cout << "20 +\n30 +\n10\n" << "----------------------------------\nTotal = " << firstNum + secondNum + thirdNum << endl << endl;
+    int firstNum, secondNum, thirdNum;
+    cout << "Enter the first number: ";
+    cin >> firstNum;
+
+    cout << "Enter the second number: ";
+    cin >> secondNum;
+
+    cout << "Enter the third number: ";
+    cin >> thirdNum;
+    cout << firstNum << "+\n" << secondNum << "+\n" << thirdNum << "\n----------------------------------\nTotal = " << firstNum + secondNum + thirdNum << endl << endl;
 
 
 
-    int myAge = 25;
+    int myAge;
+    cout << "Enter your age: ";
+    cin >> myAge;
     cout << "After 5 Years you will be " << myAge + 5 << " years old." << endl;
 
     return 0;
