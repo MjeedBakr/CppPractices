10ee68c16b433167be1b06362b1d2cae3a5c9efe
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-08-31 15:45:24 +0300

    #11 Homework: Enums

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,58 +1,43 @@
+
 #include <iostream>
 using namespace std;
 
+enum Gender{Male, Female};
+enum Status{Single, Married};
+enum Color{Red, Blue, Green, Yellow, Black, White};
+
 int main()
 {
-#include <iostream>
-    using namespace std;
+    string name = "Mohammed Abu-Hadhud.";
+    int age = 44;
+    string city = "Amman.", country = "Jordan.";
+    int monthlySalary = 5000, yearlySalary = monthlySalary * 12;
+    Gender gender = Gender::Male;
+    Status status = Status::Married;
+    Color favoriteColor = Color::Black;
 
-    struct PersonAddress {
-        string country;
-        string city;
-        string residentialArea;
-        string departmentNumber;
-    };
-
-    struct ContactInformation {
-        string email;
-        string phoneNumber;
-        string facebookAccount;
-        string twitterAccount;
-        string linkedInAccount;
-    };
-
-    struct Person {
-        string fullName;
-        short age;
-        float monthlySalary;
-        float yearlySalary = monthlySalary * 12;
-        char gender;
-        bool isMarried;
-        PersonAddress address;
-        ContactInformation contact;
-    };
+    cout << "***********************************" << endl;
+    cout << "Name: " << name << "\n";
+    cout << "Age: " << age << " Years.\n";
+    cout << "City: " << city << "\n";
+    cout << "Country: " << country << "\n";
+    cout << "Monthly Salary: " << monthlySalary << "\n";
+    cout << "Yearly Salary: " << yearlySalary << "\n";
+    cout << "Gender: " << gender << "\n";
+    cout << "Married: " << status << "\n";
+    cout << "Favorite color: " << favoriteColor << "\n";
+    cout << "***********************************" << endl << endl;
 
 
-    int main()
-    {
-        Person p1;
 
-        p1.fullName = "Ahmad Al saleh";
-        p1.age = 20;
-        p1.gender = 'M';
-        p1.isMarried = false;
-        p1.monthlySalary = 9000;
 
-        p1.address.country = "Saudi Arabia";
-        p1.address.city = "Makkah";
-        p1.address.residentialArea = "Al Awali";
-        p1.address.departmentNumber = "3007";
+    int firstNum = 20, secondNum = 30, thirdNum = 10;
+    cout << "20 +\n30 +\n10\n" << "----------------------------------\nTotal = " << firstNum + secondNum + thirdNum << endl << endl;
 
-        p1.contact.email = "ahmad2002@gmail.com";
-        p1.contact.phoneNumber = "0555566331";
-        p1.contact.facebookAccount = "@AhmadtheKing";
-        p1.contact.linkedInAccount = "@AhmadtheKing";
-        p1.contact.twitterAccount = "@AhmadtheKing";
-        return 0;
-    }
+
+
+    int myAge = 25;
+    cout << "After 5 Years you will be " << myAge + 5 << " years old." << endl;
+
+    return 0;
 }
