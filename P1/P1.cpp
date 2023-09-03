2431ac9e7d90fe3a5aa202380152bd7e49409eb2
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-03 09:50:44 +0300

    #12 Homework Casting Data

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,83 +1,28 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
-
-enum enGender{Male, Female};
-enum enMaritalStatus{Single, Married};
-enum enColor{Red, Blue, Green, Yellow, Black, White};
-
-struct stAddress {
-    string countryName;
-    string cityName;
-    string streetName;
-    string buildingNo;
-};
-
-struct stContactInfo {
-    string email;
-    string phoneNumber;
-    stAddress address;
-};
-
-
-
-
-struct stPerson {
-    string firstName;
-    string lastName;
-    short age;
-    int monthlySalary;
-    int yearlySalary = monthlySalary * 12;
-
-    enGender gender;
-    enMaritalStatus maritalStatus;
-    enColor favoriteColor;
-
-    stContactInfo contactInfo;
-};
 
 int main()
 {
-    stPerson person1;
-    person1.firstName = "Mohammed";
-    person1.lastName = "Abu-Hadhud";
-    person1.age = 44;
-    person1.monthlySalary = 12000;
+    string st1 = "43.22";
+    double dt1 = stod(st1);
+    float ft1 = stof(st1);
+    int it1 = stoi(st1);
 
-    person1.gender = enGender::Male;
-    person1.maritalStatus = enMaritalStatus::Married;
-    person1.favoriteColor = enColor::Black;
+    int n1 = 20;
+    string s1 = to_string(n1);
 
-    person1.contactInfo.email = "xy-xz@mail.com";
-    person1.contactInfo.phoneNumber = "+966521846841";
-    person1.contactInfo.address.countryName = "Jordan";
-    person1.contactInfo.address.cityName = "Amman";
-    person1.contactInfo.address.buildingNo = "3707";
-    person1.contactInfo.address.streetName = "queen street";
+    double n2 = 33.5;
+    string d2 = to_string(n2);
 
+    float n3 = 55.23;
+    string s3 = to_string(n3);
+    int i3 = n3;
+    i3 = (int) n3;
+    i3 = int(n3);
 
-    cout << "***********************************" << endl;
-    cout << "Name: " << person1.firstName << " " << person1.lastName << "\n";
-    cout << "Age: " << person1.age << " Years.\n";
-    cout << "City: " << person1.contactInfo.address.cityName << "\n";
-    cout << "Country: " << person1.contactInfo.address.countryName << "\n";
-    cout << "Monthly Salary: " << person1.monthlySalary << "\n";
-    cout << "Yearly Salary: " << person1.yearlySalary << "\n";
-    cout << "Gender: " << person1.gender << "\n";
-    cout << "Married: " << person1.maritalStatus << "\n";
-    cout << "Favorite color: " << person1.favoriteColor << "\n";
-    cout << "***********************************" << endl << endl;
-
-
-
-
-    int firstNum = 20, secondNum = 30, thirdNum = 10;
-    cout << "20 +\n30 +\n10\n" << "----------------------------------\nTotal = " << firstNum + secondNum + thirdNum << endl << endl;
-
-
-
-    int myAge = 25;
-    cout << "After 5 Years you will be " << myAge + 5 << " years old." << endl;
-
+    cout << d2 << endl;
+    
     return 0;
 }
