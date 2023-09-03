8e444082a449e819f5717361e45bbdbe3327f459
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-03 09:06:18 +0300

    11#Homework update

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -2,30 +2,70 @@
 #include <iostream>
 using namespace std;
 
-enum Gender{Male, Female};
-enum Status{Single, Married};
-enum Color{Red, Blue, Green, Yellow, Black, White};
+enum enGender{Male, Female};
+enum enMaritalStatus{Single, Married};
+enum enColor{Red, Blue, Green, Yellow, Black, White};
+
+struct stAddress {
+    string countryName;
+    string cityName;
+    string streetName;
+    string buildingNo;
+};
+
+struct stContactInfo {
+    string email;
+    string phoneNumber;
+    stAddress address;
+};
+
+
+
+
+struct stPerson {
+    string firstName;
+    string lastName;
+    short age;
+    int monthlySalary;
+    int yearlySalary = monthlySalary * 12;
+
+    enGender gender;
+    enMaritalStatus maritalStatus;
+    enColor favoriteColor;
+
+    stContactInfo contactInfo;
+};
 
 int main()
 {
-    string name = "Mohammed Abu-Hadhud.";
-    int age = 44;
-    string city = "Amman.", country = "Jordan.";
-    int monthlySalary = 5000, yearlySalary = monthlySalary * 12;
-    Gender gender = Gender::Male;
-    Status status = Status::Married;
-    Color favoriteColor = Color::Black;
+    stPerson person1;
+    person1.firstName = "Mohammed";
+    person1.lastName = "Abu-Hadhud";
+    person1.age = 44;
+    person1.monthlySalary = 12000;
+
+    person1.gender = enGender::Male;
+    person1.maritalStatus = enMaritalStatus::Married;
+    person1.favoriteColor = enColor::Black;
+
+    person1.contactInfo.email = "xy-xz@mail.com";
+    person1.contactInfo.phoneNumber = "+966521846841";
+    person1.contactInfo.address.countryName = "Jordan";
+    person1.contactInfo.address.cityName = "Amman";
+    person1.contactInfo.address.buildingNo = "3707";
+    person1.contactInfo.address.streetName = "queen street";
+
 
     cout << "***********************************" << endl;
-    cout << "Name: " << name << "\n";
-    cout << "Age: " << age << " Years.\n";
-    cout << "City: " << city << "\n";
-    cout << "Country: " << country << "\n";
-    cout << "Monthly Salary: " << monthlySalary << "\n";
-    cout << "Yearly Salary: " << yearlySalary << "\n";
-    cout << "Gender: " << gender << "\n";
-    cout << "Married: " << status << "\n";
-    cout << "Favorite color: " << favoriteColor << "\n";
+    cout << "Name: " << person1.firstName << " " << person1.lastName << "\n";
+    cout << "Age: " << person1.age << " Years.\n";
+    cout << "City: " << person1.contactInfo.address.cityName << "\n";
+    cout << "Country: " << person1.contactInfo.address.countryName << "\n";
+    cout << "Monthly Salary: " << person1.monthlySalary << "\n";
+    cout << "Yearly Salary: " << person1.yearlySalary << "\n";
+    cout << "Gender: " << person1.gender << "\n";
+    cout << "Married: " << person1.maritalStatus << "\n";
+    cout << "Favorite color: " << person1.favoriteColor << "\n";
     cout << "***********************************" << endl << endl;
 
 
