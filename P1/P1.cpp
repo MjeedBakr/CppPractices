861da417f8dc1fa1a5400fb6b71aede08511b13c
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-05 14:53:28 +0300

    #15 Homework: solve a problem using a function and a procedure

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,51 +1,8 @@
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-void displayMyCardInfo(){
-
-    std::cout << "**************************\n";
-    std::cout << "Name: Abdulmajeed Bakr.\n";
-    std::cout << "Age : 21 Years.\n";
-    std::cout << "City: Makkah.\n";
-    std::cout << "Country: Saudi Arabia.\n";
-    std::cout << "**************************\n\n\n";
-}
-
-void printSquareStars(){
-
-    std::cout << "********\n";
-    std::cout << "********\n";
-    std::cout << "********\n";
-    std::cout << "********\n\n\n";
-}
-
-void printILoveProgramming(){
-
-    std::cout << "I Love Programming!\n\n";
-    std::cout << "I promise to be the best develper ever!\n\n";
-    std::cout << "I know it will take time to practice, but I\nwill achieve my goal.\n\n";
-    std::cout << "Best Regards,\n";
-    std::cout << "Abdulmajeed Bakr.\n\n\n";
-}
-
-void printH(){
-
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "* * * * *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-}
-
-
-int main()
-{
-    displayMyCardInfo();
-    printSquareStars();
-    printILoveProgramming();
-    printH();
-
+int main() {	
+	cout << "Hi";
     return 0;
 }
