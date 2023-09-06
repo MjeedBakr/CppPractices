31037934a5355f584166743d37f983d07b9ac43a
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-06 17:59:39 +0300

    #18 Homework: update swap numbers problem solution using by ref "&" inside the parameter

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int x = 20;
-int y = 3;
-
-int myFunction()
+//14
+void swapNumbers(int& x, int& y)
 {
-	int x = 3;
-	int y = 5;
+	int temp = x;
+	x = y;
+	y = temp;
 
-	return x + y;
+	cout << x << endl;
+	cout << y << endl;
+
+
 }
 
 int main()
 {
-	int x = 40;
-	cout << "Global variable x and y = " << ::x << "\t" << ::y << endl;
-	cout << "Local variable x = " << x << endl;
-	cout << myFunction();
+	int x = 5;
+	int y = 10;
+
+	cout << x << "\n" << y << "\n" << endl;
+
+	swapNumbers(x, y);
+
+	cout << x << "\n" << y << "\n" << endl;
+
+
+
+
+	return 0;
 }
