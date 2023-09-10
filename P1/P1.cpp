c60bc48a6134edbb66c0bfd5ce3e60b29c34b546
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-10 13:18:15 +0300

    #19 Homework: working with arrays & functions

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void readGrades(float grades[3])
+{
+	cout << "Please Enter Grade1 ? \n";
+	cin >> grades[0];
+	
+	cout << "Please Enter Grade2 ? \n";
+	cin >> grades[1];
+
+	cout << "Please Enter Grade3 ? \n";
+	cin >> grades[2];
+}
+
+float calcGradesAvg(float grades[3])
+{
+	return (grades[0] + grades[1] + grades[2]) / 3;;
+}
+
+void printGradesAvg(float grades[3])
+{
+	cout << "*******************" << endl;
+	cout << "The average of grades is ";
+	cout << calcGradesAvg(grades) << endl;
+}
+
 int main()
 {
 	float grades[3];
-
-	cout << "Please Enter Grade1 ?" << endl;
-	cin >> grades[0];
-	
-	cout << "Please Enter Grade2 ?" << endl;
-	cin >> grades[1];
-
-	cout << "Please Enter Grade3 ?" << endl;
-	cin >> grades[2];
-
-	float gradesAvg = (grades[0] + grades[1] + grades[2]) / 3;
-
-	cout << "*******************" << endl;
-	cout << "The average of grades is ";
-	cout << gradesAvg << endl;
+	readGrades(grades);
+	printGradesAvg(grades);
 
 	return 0;
 }
