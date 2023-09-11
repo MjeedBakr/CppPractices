8da70e8b1946d1b137c6c17929a5607cd82eb590
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-11 11:04:39 +0300

    #22 update Homework: if- else statements with functions

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main()
+//4#
+void driverHiring()
 {
-	//4, 8, 11, 24, 49
-
-	//4#
 	short age;
 	bool hasDriverLicense;
 
@@ -24,12 +22,13 @@ int main()
 		cout << "Rejected";
 	}
 
-	//8#
-	short studentMark;
-	cout << "Enter your Mark: ";
-	cin >> studentMark;
-	
-	if (studentMark >= 50)
+}
+
+//8#
+void ifStudentsPassOrFail(short mark)
+{
+
+	if (mark >= 50)
 	{
 		cout << "Pass";
 	}
@@ -38,9 +37,11 @@ int main()
 		cout << "Fail";
 	}
 
-	//11#
-	short mark1, mark2, mark3;
+}
 
+//11#
+float avgStudentsMark(short mark1, short mark2, short mark3)
+{
 	cout << "Enter Mark1: ";
 	cin >> mark1;
 	cout << "Enter Mark2: ";
@@ -48,23 +49,13 @@ int main()
 	cout << "Enter Mark3: ";
 	cin >> mark3;
 
-	float avgMarks = (mark1 + mark2 + mark3) / 3;
-	
-	if (avgMarks >= 50)
-	{
-		cout << avgMarks << "\nPass";
-	}
-	else
-	{
-		cout << avgMarks << "\nFail";
-	}
-	
-	//24#
-    short age;
+	return avgMarks = (mark1 + mark2 + mark3) / 3;
 
-	cout << "Enter your age: ";
-	cin >> age;
+}
 
+//24#
+void ageValidation(short age)
+{
 	if (age >= 18 && age <= 45)
 	{
 		cout << "Valid Age";
@@ -74,11 +65,11 @@ int main()
 		cout << "Invalid Age";
 	}
 
-	//49#
-	string pinCode;
-	cout << "Enter your PIN code: ";
-	cin >> pinCode;
+}
 
+//49#
+void validatePinCode(string pinCode)
+{
 	if (pinCode == "1234")
 	{
 		cout << "True PIN";
@@ -88,5 +79,8 @@ int main()
 		cout << "Wrong PIN";
 	}
 
+}
+int main()
+{
 	return 0;
 }
