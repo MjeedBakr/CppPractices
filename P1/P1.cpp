8a41a9a000def80bb263b9b2ececba931fef0bd2
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-20 08:26:58 +0300

    25# Homework: for-loop

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,114 +1,93 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-//36 44 45
+//26, 27, 28, 29, 30, 32, 46
 
-int simpleCalculator(int number1, int number2, char operation)
+//26
+void printNumbersFrom1toN(int end)
 {
-
-	switch (operation)
+	for (int i = 1; i <= end; i++)
 	{
-	case '+':
-		return number1 + number2;
-		break;
-	case '-':
-		return number1 - number2;
-		break;
-	case '*':
-		return number1 * number2;
-		break;
-	case '/':
-		return number1 / number2;
-		break;
-	default:
-		return -1;
-		break;
+		cout << i << endl;
 	}
-
 }
 
-string weekDay(short dayNumber)
+//27
+void printNumbersFromNto1(int end)
 {
-
-	switch (dayNumber)
+	for (int i = end; i >= 1; i--)
 	{
-	case 1:
-		return "Sunday";
-		break;
-	case 2:
-		return "Monday";
-		break;
-	case 3:
-		return "Tuesday";
-		break;
-	case 4:
-		return "Wednesday";
-		break;
-	case 5:
-		return "Thursday";
-		break;
-	case 6:
-		return "Friday";
-		break;
-	case 7:
-		return "Saturday";
-		break;
-	default:
-		return "Wrong Day";
-		break;
+		cout << i << endl;
 	}
+}
 
-}string month(short monthNumber)
+//28
+int sumOddNumbersFrom1ToN(int number)
 {
-
-	switch (monthNumber)
+	int sum = 0;
+	for (int i = 1; i <= number; i++)
 	{
-	case 1:
-		return "January";
-		break;
-	case 2:
-		return "Feb";
-		break;
-	case 3:
-		return "Mar";
-		break;
-	case 4:
-		return "Apr";
-		break;
-	case 5:
-		return "May";
-		break;
-	case 6:
-		return "June";
-		break;
-	case 7:
-		return "Jul";
-		break;
-	case 8:
-		return "Aug";
-		break;
-	case 9:
-		return "Sep";
-		break;
-	case 10:
-		return "Oct";
-		break;
-	case 11:
-		return "Nov";
-		break;
-	case 12:
-		return "December";
-	default:
-		return "Wrong Month";
-		break;
+		if (i % 2 != 0)
+			sum += i;
 	}
+	return sum;
+}
 
+//29
+int sumEvenNumbersFrom1ToN(int number)
+{
+	int sum = 0;
+	for (int i = 1; i <= number; i++)
+	{
+		if (i % 2 == 0)
+			sum += i;
+	}
+	return sum;
+}
+
+//30
+int calcFactorial(int number)
+{
+	int result = 1;
+	for (int i = number; i > 0; i--)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+//32
+int calcPower(int number, int m)
+{
+	for (int i = 2; i < m; i++)
+	{
+		number *= number;
+	}
+	return number;
+}
+
+//46  Capital letters: 65 - 90 
+void printCapitalLetters()
+{
+	for (int i = 65; i <= 90; i++)
+	{
+		cout << char(i) << endl;
+	}
+}
+
+//46  Small letters: 97 - 122 
+void printSmallLetters()
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		cout << char(i) << endl;
+	}
 }
 
 
 int main()
 {
-
-
+	printCapitalLetters();
+	printSmallLetters();
 	return 0;
 }
