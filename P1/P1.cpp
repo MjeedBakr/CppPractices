6360a4dead77113a8ae09ee99e5980eb5f52f929
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-20 09:24:26 +0300

    26# Homework: update homework 21# with arrays and loops

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,93 +1,66 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//26, 27, 28, 29, 30, 32, 46
-
-//26
-void printNumbersFrom1toN(int end)
+struct strPersonInfo
 {
-	for (int i = 1; i <= end; i++)
-	{
-		cout << i << endl;
-	}
+	string firstName;
+	string lastName;
+	short age;
+	string phoneNumber;
+};
+
+void readPersonInfo(strPersonInfo& person)
+{
+	cout << "Enter your first name: ";
+	cin >> person.firstName;
+	cout << "Enter your last name: ";
+	cin >> person.lastName;
+	cout << "Enter your age: ";
+	cin >> person.age;
+	cout << "Enter your phone number: ";
+	cin >> person.phoneNumber;
 }
 
-//27
-void printNumbersFromNto1(int end)
+void printPersonInfo(strPersonInfo person)
 {
-	for (int i = end; i >= 1; i--)
-	{
-		cout << i << endl;
-	}
+	cout << "First Name: " << person.firstName << "\n";
+	cout << "Last Name: " << person.lastName << "\n";
+	cout << "Age: " << person.age << " Years.\n";
+	cout << "Phone: " << person.phoneNumber << ".\n";
 }
 
-//28
-int sumOddNumbersFrom1ToN(int number)
+void readPeopleInfo(strPersonInfo person[], int length)
 {
-	int sum = 0;
-	for (int i = 1; i <= number; i++)
+	for (int i = 0; i <= length - 1; i++)
 	{
-		if (i % 2 != 0)
-			sum += i;
+		cout << "For the person (" << i + 1 << ")\n";
+		readPersonInfo(person[i]);
+		cout << endl;
 	}
-	return sum;
+	cout << "\n" << endl;
 }
 
-//29
-int sumEvenNumbersFrom1ToN(int number)
+void printPeopleInfo(strPersonInfo person[], int length)
 {
-	int sum = 0;
-	for (int i = 1; i <= number; i++)
+	for (int i = 0; i <= length - 1; i++)
 	{
-		if (i % 2 == 0)
-			sum += i;
-	}
-	return sum;
-}
-
-//30
-int calcFactorial(int number)
-{
-	int result = 1;
-	for (int i = number; i > 0; i--)
-	{
-		result *= i;
-	}
-	return result;
-}
-
-//32
-int calcPower(int number, int m)
-{
-	for (int i = 2; i < m; i++)
-	{
-		number *= number;
-	}
-	return number;
-}
-
-//46  Capital letters: 65 - 90 
-void printCapitalLetters()
-{
-	for (int i = 65; i <= 90; i++)
-	{
-		cout << char(i) << endl;
+		cout << "********************* \n";
+		printPersonInfo(person[i]);
+		cout << "\n" << endl;
+		cout << "********************* \n";
 	}
 }
-
-//46  Small letters: 97 - 122 
-void printSmallLetters()
-{
-	for (int i = 97; i <= 122; i++)
-	{
-		cout << char(i) << endl;
-	}
-}
-
 
 int main()
 {
-	printCapitalLetters();
-	printSmallLetters();
+	int numberOfPeople = 0;
+	cout << "Enter the number of people you want to add:\n";
+	cin >> numberOfPeople;
+
+	strPersonInfo people[100];
+
+	readPeopleInfo(people, numberOfPeople);
+	printPeopleInfo(people, numberOfPeople);
+
 	return 0;
 }
