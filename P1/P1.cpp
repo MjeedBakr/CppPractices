b3cbe5e33b2585cd2dcc115958d0657732d0ac25
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-22 15:06:42 +0300

    29# Homework: continue with for loop

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,138 +1,16 @@
 #include <iostream>
 using namespace std;
-//26, 27, 28, 29, 30, 32, 37, 46, 50
 
-//26
-void printNumberFrom1ToN(int number)
-{
-	int counter = 1;
-	while (number >= 1)
-	{
-		cout << counter << endl;
-		number--;
-		counter++;
-	}
-}
-
-//27
-void printNumberFromNTo1(int number)
-{
-	while (number >= 1)
-	{
-		cout << number << endl;
-		number--;
-	}
-}
-
-//28
-int sumOddNumbersFrom1ToN(int number)
-{
-	int sum = 0;
-	while (number >= 1)
-	{
-		if (number % 2 != 0)
-		{
-			sum += number;
-		}
-		number--;
-	}
-	return sum;
-}
-
-//29
-int sumEvenNumbersFrom1ToN(int number)
-{
-	int sum = 0;
-	while (number >= 1)
-	{
-		if (number % 2 == 0)
-			sum += number;
-		number--;
-	}
-	return sum;
-}
-
-//30
-void factorial()
-{
-	int number;
-	cout << "Please Enter a number: ";
-	cin >> number;
-	while (number <= 0)
-	{
-		cout << "Please Enter a positive number: ";
-		cin >> number;
-	}
-	int result = 1;
-	while (number >= 1)
-	{
-		result *= number;
-		number--;
-	}
-
-	cout << result;
-}
-
-//32
-int power(int number, int m)
-{
-	while (m > 2)
-	{
-		number *= number;
-		m--;
-	}
-	return number;
-}
-
-//37
-void sumUntil99()
-{
-	int sum = 0;
-	int number = 0;
-	while (number != -99)
-	{
-		sum += number;
-		cout << "Enter a number to add to the sum\n";
-		cin >> number;
-	}
-	cout << sum;
-}
-
-//46
-void printLetters()
-{
-	short counter = 65;
-	while (counter <= 90)
-	{
-		cout << char(counter) << endl;;
-		counter++;
-	}
-}
-
-//50
-void displayBalance()
-{
-	string PIN;
-	cout << "Please Enter your PIN\n";
-	cin >> PIN;
-
-	int failCounter = 3;
-	while (PIN != "1234" && failCounter >= 1)
-	{
-		cout << "Wrong PIN, Please Enter your PIM again\n";
-		cin >> PIN;
-		failCounter--;
-		if (failCounter == 0)
-			cout << "Card is locked!";
-	}
-	if (PIN == "1234")
-	{
-		cout << "Your Balance is: 7500";
-	}
-}
 int main()
 {
-	factorial();
 
+	int myArray[10] = { 10, 20, 44, 55, 33, 22, 99, 100 };
+
+	for (int i = 0; i <= 10 - 1; i++)
+	{
+		if (myArray[i] == 20)
+			continue;
+		cout << myArray[i] << endl;
+	}
 	return 0;
 }
