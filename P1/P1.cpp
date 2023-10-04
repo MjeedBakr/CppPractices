a6114518933d703b75b2a4e7e86d7930bd159c15
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-04 21:16:33 +0300

    03/2#: Homework check if the number is perfect

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-enum enPrimeOrNot {Prime = 1, NotPrime = 2};
+enum enPerfectOrNot {Perfect = 1, NotPerfect = 2};
 
 int readPositiveNumber(string message)
 {
@@ -15,28 +15,31 @@ int readPositiveNumber(string message)
 		return number;
 }
 
-enPrimeOrNot checkPrime(int number)
+enPerfectOrNot checkPerfect(int number)
 {
 	int m = round(number / 2);
-	for (int counter = 2; counter <= m; counter++)
+	int sum = 0;
+	for (int counter = 1; counter <= m; counter++)
 		if (number % counter == 0)
-			return enPrimeOrNot::NotPrime;
+			sum += counter;
 
-	return enPrimeOrNot::Prime;
+	if (sum == number)
+		return enPerfectOrNot::Perfect;
+
+	return enPerfectOrNot::NotPerfect;
 }
 
-void printPrimeNumbersFrom1ToN(int number)
+void printPerfectNumber(int number)
 {
-	cout << "\nPrime number from 1 To " << to_string(number) << " are: " << endl;
-	for (int i = 1; i <= number; i++)
-	{
-		if (checkPrime(i) == enPrimeOrNot::Prime)
-			cout << i << endl;
-	}
+	if (checkPerfect(number) == enPerfectOrNot::Perfect)
+		cout << number << " Is a Pefect Number.";
+	else
+		cout << number << " Is not a Perfect Number.";
+
 }
 
 int main()
 {
-	printPrimeNumbersFrom1ToN(readPositiveNumber("PLease enter a positive number to prine all prime number from 1 to n: "));
+	printPerfectNumber(readPositiveNumber("PLease enter a positive number to check if it is Perfect or Not: "));
 	return 0;
 }
