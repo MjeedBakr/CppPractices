c8c52aa5fa33e3991ae9ee151c325bb4f1ae4f27
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-06 17:44:39 +0300

    13/2#: Homework number pattern

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -15,9 +15,9 @@ int readPositiveNumber(string message)
 
 void printInvertedNumberPattern(int number)
 {
-	for (int i = number; i >= 1; i--)
+	for (int i = 1; i <= number; i++)
 	{
-		for (int j = i; j >= 1; j--)
+		for (int j = 1; j <= i; j++)
 			cout << i << " ";
 		cout << endl;
 	}
