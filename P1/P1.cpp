f3a14fd814aa60f56d71c1eea22447f745b15ccf
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-07 15:05:27 +0300

    20/2#: Homework random small letter, capital letter, special c, and digit in order

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 using namespace std;
 
+enum enCharType {SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4};
+
 //function to generate random number
 int randomNumber(int from, int to)
 {
@@ -9,13 +11,32 @@ int randomNumber(int from, int to)
 	return randNum;
 }
 
+char randomCharacter(enCharType charType)
+{
+	switch (charType)
+	{
+	case SmallLetter:
+		return char(randomNumber(97, 122));
+	case CapitalLetter:
+		return char(randomNumber(65, 90));
+	case SpecialCharacter:
+		return char(randomNumber(33, 47));
+	case Digit:
+		return char(randomNumber(48, 57));
+	default:
+		break;
+	}
+}
 int main()
 {
 	//Seeds the random number generator in C++, called only once
 	srand((unsigned)time(NULL));
 
-	cout << randomNumber(1, 10) << endl;
-	cout << randomNumber(1, 10) << endl;
-	cout << randomNumber(1, 10) << endl;
+	cout << randomCharacter(enCharType::SmallLetter) << endl;
+	cout << randomCharacter(enCharType::CapitalLetter) << endl;
+	cout << randomCharacter(enCharType::SpecialCharacter) << endl;
+	cout << randomCharacter(enCharType::Digit) << endl;
+
+
 	return 0;
 }
