90e14fafb47df5bb109e90f3303429493fb0e076
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-09 11:31:58 +0300

    31/2#: Homework shuffle ordered array

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -27,18 +27,44 @@ void printArray(int arr[100], int arrayLength)
 	cout << endl;
 }
 
-void fillArraywithRandomNumbers(int arr[100], int arrayLength, int from, int to)
+void fillArraywithNumbersFrom1ToN(int arr[100], int arrayLength)
 {
 
 	for (int i = 0; i <= arrayLength - 1; i++)
-		arr[i] = randomNumber(from, to);
+		arr[i] = i + 1;
 
 }
 
-void sumArrays(int arr[100], int arr2[100], int arrayLength, int arrSum[100])
+bool arrayContainsTheNumber(int arr[100], int arrayLength, int number)
 {
 	for (int i = 0; i <= arrayLength - 1; i++)
-		arrSum[i] = arr[i] + arr2[i];
+		if (arr[i] == number)
+			return true;
+
+	return false;
+}
+
+void copyArray(int oldArray[100], int newArray[100], int arrayLength)
+{
+	for (int i = 0; i <= arrayLength - 1; i++)
+		newArray[i] = oldArray[i];
+
+}
+
+void arrayShuffle(int arr[100], int arrayLength)
+{
+	int arrResult[100];
+	int randomNum;
+	for (int i = 0; i <= arrayLength - 1; i++)
+	{
+		do
+		{
+			randomNum = randomNumber(0, arrayLength - 1);
+
+		} while (arrayContainsTheNumber(arrResult, arrayLength, arr[randomNum]));
+		arrResult[i] = arr[randomNum];
+	}
+	copyArray(arrResult, arr, arrayLength);
 }
 
 int main()
@@ -46,20 +72,16 @@ int main()
 	//Seeds the random number generator in C++, called only once
 	srand((unsigned)time(NULL));
 	int arrayLength = readPositiveNumber("How many elements: ");
-	int arr[100], arr2[100], arrSum[100];
+	int arr[100];
 
-	fillArraywithRandomNumbers(arr, arrayLength, 1, 100);
-	fillArraywithRandomNumbers(arr2, arrayLength, 1, 100);
+	fillArraywithNumbersFrom1ToN(arr, arrayLength);
 
-	cout << "\nArray 1 Elements: ";
+	cout << "\nArray elements before shuffle: \n";
 	printArray(arr, arrayLength);
 
-	cout << "\nArray 2 Elements: ";
-	printArray(arr2, arrayLength);
-
-	cout << "\nSum of array1 and array2 elements: ";
-	sumArrays(arr, arr2, arrayLength, arrSum);
-	printArray(arrSum, arrayLength);
+	arrayShuffle(arr, arrayLength);
+	cout << "\nArray elements after shuffle: \n";
+	printArray(arr, arrayLength);
 
 
 
