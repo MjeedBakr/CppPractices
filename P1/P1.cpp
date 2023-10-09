fa2531888d31a50afb0bff2c944fcc65c3b3bc54
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-09 11:40:52 +0300

    31/2#: Homework update shuffle ordered array

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -35,15 +35,6 @@ void fillArraywithNumbersFrom1ToN(int arr[100], int arrayLength)
 
 }
 
-bool arrayContainsTheNumber(int arr[100], int arrayLength, int number)
-{
-	for (int i = 0; i <= arrayLength - 1; i++)
-		if (arr[i] == number)
-			return true;
-
-	return false;
-}
-
 void copyArray(int oldArray[100], int newArray[100], int arrayLength)
 {
 	for (int i = 0; i <= arrayLength - 1; i++)
@@ -51,28 +42,29 @@ void copyArray(int oldArray[100], int newArray[100], int arrayLength)
 
 }
 
+void swap(int& a, int& b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
 void arrayShuffle(int arr[100], int arrayLength)
 {
-	int arrResult[100];
-	int randomNum;
 	for (int i = 0; i <= arrayLength - 1; i++)
 	{
-		do
-		{
-			randomNum = randomNumber(0, arrayLength - 1);
-
-		} while (arrayContainsTheNumber(arrResult, arrayLength, arr[randomNum]));
-		arrResult[i] = arr[randomNum];
+		swap(arr[randomNumber(0, arrayLength - 1)], arr[randomNumber(0, arrayLength - 1)]);
 	}
-	copyArray(arrResult, arr, arrayLength);
 }
 
 int main()
 {
 	//Seeds the random number generator in C++, called only once
 	srand((unsigned)time(NULL));
-	int arrayLength = readPositiveNumber("How many elements: ");
+
 	int arr[100];
+	int arrayLength = readPositiveNumber("How many elements: ");
 
 	fillArraywithNumbersFrom1ToN(arr, arrayLength);
 
@@ -80,6 +72,7 @@ int main()
 	printArray(arr, arrayLength);
 
 	arrayShuffle(arr, arrayLength);
+
 	cout << "\nArray elements after shuffle: \n";
 	printArray(arr, arrayLength);
 
