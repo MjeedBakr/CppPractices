352244bc100480839be4809de6fbc92a7b174dd9
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-12 17:29:25 +0300

    36/2#: Homework semi dynamic array

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -2,77 +2,50 @@
 #include <cstdlib>
 using namespace std;
 
-int randomNumber(int from, int to)
-{
-	int randNum = rand() % (to - from + 1) + from;
-	return randNum;
-}
-
-void fillArrayWithRandomNumbers(int arr[100], int &arrayLength)
-{
-	cout << "\nHow many elements do you want in your array: \n";
-	cin >> arrayLength;
-
-	for (int i = 0; i <= arrayLength; i++)
-	{
-		arr[i] = randomNumber(1, 100);
-	}
-}
-
 void printArray(int arr[100], int arrayLength)
 {
-	cout << "\nArray elements: \n";
+	cout << "\nArray Length: " << arrayLength;
+	cout << "\nArray elements: ";
 	for (int i = 0; i <= arrayLength - 1; i++)
 		cout << arr[i] << " ";
 
 	cout << endl;
 }
 
-short findNumberPositionInArray(int arr[100], int arrayLength, int number)
-{
-	for (int i = 0; i <= arrayLength; i++)
-	{
-		if (arr[i] == number)
-			return i;
-	}
-
-	return -1;
-}
-
-bool isNumberInArray(int arr[100], int arrayLength, int number)
-{
-	return findNumberPositionInArray(arr, arrayLength, number) != -1;
-}
-
 int readNumber()
 {
 	int number;
 
-	cout << "\nPlease enter a number to search for: \n";
+	cout << "\nPlease enter a number: \n";
 	cin >> number;
 
 	return number;
 }
 
+void addNumbersToArray(int arr[100], int& arrayLength)
+{
+	short choice = 0;
+	do
+	{
+		arr[arrayLength] = readNumber();
+		arrayLength++;
+
+		cout << "Do you want to add more numbers? [0]:No, [1]:Yes? ";
+		cin >> choice;
+	} while (choice == 1);
+
+}
+
 int main()
 {
-	//Seeds the random number generator in C++, called only once
-	srand((unsigned)time(NULL));
+
 
 	int arr[100];
 	int arrayLength = 0;
 
-	fillArrayWithRandomNumbers(arr, arrayLength);
+	addNumbersToArray(arr, arrayLength);
 
 	printArray(arr, arrayLength);
-	
-	int number = readNumber();
-	cout << "\nNumber you are looking for is: " << number << endl;
-
-	if (!isNumberInArray(arr, arrayLength, number))
-		cout << "No, The number is not found :-(" << endl;
-	else
-		cout << "Yes, The number is found :-)" << endl;
 
 
 	return 0;
