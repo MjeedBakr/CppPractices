9a8ad29d1335623673606a7f69be05bc207d1786
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-14 19:30:02 +0300

    46/2#: Homework myABS function

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,65 +1,33 @@
 ﻿#include <iostream>
-#include <cstdlib>
+#include <cmath>
 using namespace std;
 
-int randomNumber(int from, int to)
+float readNumber()
 {
-	int randNum = rand() % (to - from + 1) + from;
-	return randNum;
+	float number;
+
+	cout << "\nEnter a number: ";
+	cin >> number;
+
+	return number;
 }
 
-void fillArrayWithRandomNumbers(int arr[100], int& arrayLength)
-{
-	cout << "\nHow many elements do you want in your array: \n";
-	cin >> arrayLength;
-
-	for (int i = 0; i <= arrayLength; i++)
-	{
-		arr[i] = randomNumber(-100, 100);
-	}
-}
-
-void printArray(int arr[100], int arrayLength)
-{
-	for (int i = 0; i <= arrayLength - 1; i++)
-		cout << arr[i] << " ";
-
-	cout << endl;
-}
-
-bool isNegative(int number)
+float myAbs(float number)
 {
 	if (number < 0)
-		return true;
+		return number * -1;
 
-	return false;
-}
-
-int countNegative(int arr[100], int arrayLength)
-{
-	int counter = 0;
-	for (int i = 0; i < arrayLength; i++)
-		if (isNegative(arr[i]))
-			counter++;
-
-	return counter;
+	return number;
 }
 
 int main()
 {
-	//Seeds the random number generator in C++, called only once
-	srand((unsigned)time(NULL));
 
-	int arr[100];
-	int arrayLength = 0;
+	int number = readNumber();
 
-	fillArrayWithRandomNumbers(arr, arrayLength);
+	cout << "My abs Result : " << myAbs(number) << endl;
 
-	cout << "\nArray elements: ";
-	printArray(arr, arrayLength);
-
-	cout << "\nNegative numbers count is: ";
-	cout << countNegative(arr, arrayLength) << endl;
+	cout << "C++ abs Result : " << abs(number) << endl;
 
 
 	return 0;
