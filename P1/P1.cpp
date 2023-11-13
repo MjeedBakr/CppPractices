af7bd41086420aba43c4efb87c58506dfb4849a7
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-11-13 13:41:41 +0300

    Function Overloading

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,14 +1,31 @@
 ﻿#include<iostream>
 using namespace std;
 
-int sum(int a, int b, int c = 0, int d = 0)
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+int sum(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+int sum(int a, int b, int c, int d)
 {
 	return a + b + c + d;
 }
 
+double sum(double a, double b)
+{
+	return a + b;
+}
+
+
 int main() 
 {
 	cout << sum(5, 6) << endl;
+	cout << sum(5.1, 6.4) << endl;
 	cout << sum(5, 6, 7) << endl;
 	cout << sum(5, 6, 7, 8) << endl;
 
