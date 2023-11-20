933df3325fe70e0e0efe15deeb5e8caec5e8cca6
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-11-20 14:49:25 +0300

    printf float formatting

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,24 +1,25 @@
-﻿#include<iostream>
+﻿#include <iostream>
+#include <cstdio>
+
 using namespace std;
 
-int main() 
-{
+int main() {
+    float PI = 3.14159265;
 
-	int page = 1, totalPages = 10;
+    // Precision specification
+    printf("Precision specification of %.*f\n", 1, PI);
+    printf("Precision specification of %.*f\n", 2, PI);
+    printf("Precision specification of %.*f\n", 3, PI);
+    printf("Precision specification of %.*f\n", 4, PI);
 
-	//print string and int variable
-	printf("The page number = %d \n", page);
-	printf("You are in PAge %d of %d \n", page, totalPages);
+    float x = 7.0, y = 9.0;
+    printf("\nThe float division is: %.3f / %.3f = %.3f \n\n", x, y, x / y);
 
-	//width specification
-	printf("The page number = %0*d \n", 2, page);
-	printf("The page number = %0*d \n", 3, page);
-	printf("The page number = %0*d \n", 4, page);
-	printf("The page number = %0*d \n", 5, page);
+    double d = 12.45;
+    printf("The double value is: %.3f \n", d);
+    printf("The double value is: %.4f \n", d);
 
-	int num1 = 20, num2 = 30;
-	printf("The result of %d + %d = %d \n", num1, num2, num1 + num2);
-
-	return 0;
+    return 0;
 }
+
 
