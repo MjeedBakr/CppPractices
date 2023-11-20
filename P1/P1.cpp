34f81f56c8de01d58aa07499fd873cdcda755fc2
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-11-20 14:14:34 +0300

    auto variables

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,38 +1,13 @@
 ﻿#include<iostream>
 using namespace std;
 
-void printNumberMaxToMin(int max, int min)
-{
-	if (max >= min)
-	{
-		cout << max << endl;
-		printNumberMaxToMin(max - 1, min);
-	}
-}
-
-void printNumberMinToMax(int max, int min)
-{
-	if (min <= max)
-	{
-		cout << min << endl;
-		printNumberMinToMax(max, min + 1);
-	}
-}
-
-int MyPower(int base, int power)
-{
-	if (power == 0)
-		return 1;
-	else
-	{
-		return (base * MyPower(base, power - 1));
-	}
-}
-
 int main() 
 {
 
-	cout << MyPower(5, 4);
+	auto x = 10;
+	auto name = "Abdulmajeed";
+	auto y = 10.49;
+
 	return 0;
 }
 
