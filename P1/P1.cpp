69d3c6c830180891081afc635095ba0b81fe294d
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-11-20 14:37:22 +0300

    printf

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -4,9 +4,20 @@ using namespace std;
 int main() 
 {
 
-	auto x = 10;
-	auto name = "Abdulmajeed";
-	auto y = 10.49;
+	int page = 1, totalPages = 10;
+
+	//print string and int variable
+	printf("The page number = %d \n", page);
+	printf("You are in PAge %d of %d \n", page, totalPages);
+
+	//width specification
+	printf("The page number = %0*d \n", 2, page);
+	printf("The page number = %0*d \n", 3, page);
+	printf("The page number = %0*d \n", 4, page);
+	printf("The page number = %0*d \n", 5, page);
+
+	int num1 = 20, num2 = 30;
+	printf("The result of %d + %d = %d \n", num1, num2, num1 + num2);
 
 	return 0;
 }
