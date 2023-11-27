ab6a30684287f7de34c171fc5b56637448643e16
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-11-27 14:08:26 +0300

    Vector Initialize

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,34 +1,16 @@
 ﻿#include <iostream>
-#include <iomanip>
+#include <vector>
 
 using namespace std;
 
-void fill2DimensionArray(int arr[10][10])
-{
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-		{
-			arr[i][j] = (i + 1) * (j + 1);
-		}
-}
-
-void print2DimensionArray(int arr[10][10])
-{
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			printf("%0*d ", 2, arr[i][j]);
-		}
-		cout << endl;
-	}
-}
 int main() {
 
-	int x[10][10];
+	vector <int> vNumbrs = { 1, 2, 3, 4, 5, 6, 7 };
 
-	fill2DimensionArray(x);
-	print2DimensionArray(x);
+	for (int &number : vNumbrs)
+	{
+		cout << number << " ";
+	}
 
 
 
