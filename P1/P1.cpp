70563813400d6c07050b228ead96a680e76bbff0
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-12-02 16:43:14 +0300

    creating references

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -3,62 +3,27 @@
 
 using namespace std;
 
-struct stEmployee
-{
-	string firstName;
-	string lastName;
-	int salary;
-};
 
-void readEmployeesInfo(vector <stEmployee> &vEmployees)
-{
-	
-	char enterAnotherEmployee;
-
-	do
-	{
-		cout << "\n";
-		stEmployee tempEmployee;
-
-		cout << "Please enter employee first name: ";
-		cin >> tempEmployee.firstName;
-
-		cout << "Please enter employee last name: ";
-		cin >> tempEmployee.lastName;
-
-		cout << "Please enter employee salary: ";
-		cin >> tempEmployee.salary;
-
-		vEmployees.push_back(tempEmployee);
-		
-		cout << "Do you want to enter another Employee info Y:Yes N:No? ";
-		cin >> enterAnotherEmployee;
-	} while (enterAnotherEmployee == 'y' || enterAnotherEmployee == 'Y');
-
-}
-
-void printEmployeesInfo(vector <stEmployee> &vEmployees) //put it by ref or it will copy the whole vector
-{
-	cout << "\n";
-	for (stEmployee &employee : vEmployees)
-	{
-		cout << "First Name: " << employee.firstName << "\n";
-		cout << "Last  Name: " << employee.lastName << "\n";
-		cout << "Salary    : " << employee.salary << "\n";
-
-		cout << endl;
-	}
-}
 
 int main() {
 
-	vector <stEmployee> vEmployees;
-	readEmployeesInfo(vEmployees);
-	printEmployeesInfo(vEmployees);
+	int a = 10;
+	int& x = a;
+
+	cout << &a << endl;
+	cout << &x << endl;
+
+	cout << a << endl;
+	cout << x << endl;
+
+	x = 20;
+
+	cout << a << endl;
+	cout << x << endl;
 
 
 
-
+	return 0;
 
 
 
