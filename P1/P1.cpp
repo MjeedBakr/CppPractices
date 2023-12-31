44c4e3e9239e5cf6a5cd71c46be8557f4fa93734
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-12-31 08:51:02 +0300

    change elements

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -7,10 +7,30 @@ int main() {
 
 	vector <int> numbers{ 1, 3, 5, 7, 9 };
 
-	//You can use both methods
-	cout << numbers[0] << endl;
-	cout << numbers.at(0) << endl;
+	cout << "Initial Vector: ";
 
+	for (const int& i : numbers)
+	{
+		cout << i << " ";
+	}
+
+	cout << "\nUpdated Vector: ";
+
+	for (int& i : numbers)
+	{
+		i = 20;
+		cout << i << " ";
+	}
+
+	numbers[0] = 10;
+	numbers.at(2) = 30;
+
+	cout << "\nUpdated Vector: ";
+
+	for (const int& i : numbers)
+	{
+		cout << i << " ";
+	}
 
 
 	return 0;
