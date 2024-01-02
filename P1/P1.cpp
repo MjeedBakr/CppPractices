8ea63f4aa9e77fd21d0378c050ae1de877efe5c4
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-01-02 14:46:43 +0300

    char Object methods

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -5,41 +5,18 @@ using namespace std;
 
 int main() {
 
-	string name = "My Name is Abdulmajeed Bakr";
+	char x;
+	char y;
 
+	x = toupper('a'); //the method returns the ascii code for upper a
+	y = tolower('B'); //the method returns the ascii code for lower B
 	
-	cout << name.length() << endl;
+	cout << isupper('A') << endl; //returns 0 if false and any number if true
+	cout << islower('a') << endl; //returns 0 if false and any number if true
+	cout << isdigit('5') << endl; //returns 0 if false and any number if true
 
-	//return the letter at index 11
-	cout << name.at(11) << endl;
-
-	//add it to the end of the string
-	cout << name.append(" Mohammed.") << endl;
-
-	//add it to position 27
-	cout << name.insert(27, " Bin") << endl;
-
-	//return 16 character after index 11
-	cout << name.substr(11, 16) << endl;
-
-	//add | to the end of the string
-	name.push_back('|');
-	cout << name << endl;
-
-	//delete the last character
-	name.pop_back();
-	cout << name << endl;
-
-	cout << name.find("Bakr") << endl;
-	cout << name.find("Said") << endl;// returns a long random number
-	
-	if (name.find("Said") == name.npos) //npos : Not in position
-		cout << "Ali is not found" << endl;
-
-	//Clear or delete all the characters
-	name.clear();
-	cout << name << endl;
-
+	//Punctuation marks are:  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+	cout << ispunct('/') << endl; //returns 0 if false and any number if true
 
 	return 0;
 
