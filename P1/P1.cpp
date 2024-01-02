659f13302a34125b4d9bb6ffc80ac73f230bd4c6
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-01-02 14:28:54 +0300

    String object methods

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,22 +1,44 @@
 ﻿#include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 
 int main() {
 
-	vector <int> numbers{ 1, 3, 5, 7, 9 };
+	string name = "My Name is Abdulmajeed Bakr";
 
-	try 
-	{
-	cout << numbers.at(5);
+	
+	cout << name.length() << endl;
 
-	}
-	catch (...)
-	{
-		cout << "Out of pound\n";
-	}
+	//return the letter at index 11
+	cout << name.at(11) << endl;
 
+	//add it to the end of the string
+	cout << name.append(" Mohammed.") << endl;
+
+	//add it to position 27
+	cout << name.insert(27, " Bin") << endl;
+
+	//return 16 character after index 11
+	cout << name.substr(11, 16) << endl;
+
+	//add | to the end of the string
+	name.push_back('|');
+	cout << name << endl;
+
+	//delete the last character
+	name.pop_back();
+	cout << name << endl;
+
+	cout << name.find("Bakr") << endl;
+	cout << name.find("Said") << endl;// returns a long random number
+	
+	if (name.find("Said") == name.npos) //npos : Not in position
+		cout << "Ali is not found" << endl;
+
+	//Clear or delete all the characters
+	name.clear();
+	cout << name << endl;
 
 
 	return 0;
