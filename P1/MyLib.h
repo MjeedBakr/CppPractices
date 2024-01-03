fec5699750a63ac604e5c8e5c31cce40c6163550
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-01-03 10:26:24 +0300

    Read content from file

--- P1/MyLib.h
+++ P1/MyLib.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<fstream>
 #include "MyLib.h"
 
 
@@ -200,5 +201,25 @@ namespace MyMatrix
 			printf("Row %d Sum = %d\n", i + 1, rowSum(arr, i, columns));
 		}
 		cout << endl;
+	}
+}
+
+namespace MyFile
+{
+	void printFileContent(string fileName)
+	{
+		fstream myFile;
+		myFile.open(fileName, ios::in); //Activate Read Mode
+
+		if (myFile.is_open())
+		{
+			string lineContent;
+
+			while (getline(myFile, lineContent))
+			{
+				cout << lineContent << endl;
+			}
+			myFile.close();
+		}
 	}
 }
--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,64 +1,30 @@
 ﻿#include <iostream>
-#include <iomanip>
-#include "MyLib.h"
+#include <fstream>
+#include<string>
+
 
 using namespace std;
-using namespace MyRandom;
 
-void fillMatrixWithRandomNumbers(int arr[3][3], short rows, short columns)
+void printFileContent(string fileName)
 {
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < columns; j++)
-			arr[i][j] = randomNumber(1, 100);
-	}
-}
+	fstream myFile;
+	myFile.open(fileName, ios::in); //Activate Read Mode
 
-void printMatrix(int arr[3][3], short rows, short columns)
-{
-	for (int i = 0; i < rows; i++)
+	if (myFile.is_open())
 	{
-		for (int j = 0; j < columns; j++)
-			cout << arr[i][j] << "\t";
-		cout << "\n";
-	}
-	cout << endl;
-}
+		string lineContent;
 
-int rowSum(int arr[3][3], short rowNumber, short columns)
-{
-	int rowSum = 0;
-	for (int i = 0; i < columns; i++)
-	{
-		rowSum += arr[rowNumber][i];
+		while (getline(myFile, lineContent))
+		{
+			cout << lineContent << endl;
+		}
+		myFile.close();
 	}
-
-	return rowSum;
-}
-
-void printRowMatrixSum(int arr[3][3], short rows, short columns)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		printf("Row %d Sum = %d\n", i+1, rowSum(arr, i, columns));
-	}
-	cout << endl;
 }
 
 int main() {
 
-	srand((unsigned)time(NULL));
-
-	int array[3][3];
-
-	fillMatrixWithRandomNumbers(array, 3, 3);
-
-	cout << "The following is 3 x 3 Random Matrix:\n";
-	printMatrix(array, 3, 3);
-
-	cout << "The following is sum of each row in matrix:\n";
-	printRowMatrixSum(array, 3, 3);
-
+	printFileContent("myFile.txt");
 	return 0;
 
 
