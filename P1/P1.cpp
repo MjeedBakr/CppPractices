890573429af77c4ae67f2ab4532626d74bccf3a1
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-01-13 19:10:18 +0300

    3/3#: Homework sum each row in seprate array

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,33 +1,81 @@
-﻿#pragma warning(disable : 4996)
-
-#include <iostream>
-#include <ctime>
+﻿#include <iostream>
+#include <iomanip>
+#include "MyLib.h"
 
 using namespace std;
+using namespace MyRandom;
 
-/*  
-  int tm_sec; // seconds of minutes from 0 to 61  
-  int tm_min; // minutes of hour from 0 to 59  
-  int tm_hour; // hours of day from 0 to 24  
-  int tm_mday; // day of month from 1 to 31  
-  int tm_mon; // month of year from 0 to 11  
-  int tm_year; // year since 1900  
-  int tm_wday; // days since sunday  
-  int tm_yday; // days since January 1st  
-  int tm_isdst; // hours of daylight savings time
-*/
-
-int main()
+void fillMatrixWithRandomNumbers(int arr[3][3], short rows, short columns)
 {
-	time_t t = time(0);   // get time now
-	tm* now = localtime(&t); 
-	cout << "Year: " << now->tm_year + 1900 << endl; 
-	cout << "Month: " << now->tm_mon + 1 << endl; 
-	cout << "Day: " << now->tm_mday << endl; 
-	cout << "Hour: " << now->tm_hour << endl; 
-	cout << "Min: " << now->tm_min << endl; 
-	cout << "Second: " << now->tm_sec << endl; 
-	cout << "Week Day (Days since sunday): " << now->tm_wday << endl; 
-	cout << "Year Day (Days since Jan 1st): " << now->tm_yday << endl; 
-	cout << "hours of daylight savings:" << now->tm_isdst << endl; 
-} 
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+			arr[i][j] = randomNumber(1, 100);
+	}
+}
+
+void printMatrix(int arr[3][3], short rows, short columns)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+			cout << arr[i][j] << "\t";
+		cout << "\n";
+	}
+	cout << endl;
+}
+
+int arraySum(int arr[3], short arrayLength)
+{
+	int sum = 0;
+	for (int i = 0; i < arrayLength; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+void saveRowInArray(int arr[3][3], short rowNumber, short columns, int rowResult[3])
+{
+	for (int i = 0; i < columns; i++)
+	{
+		rowResult[i] = arr[rowNumber - 1][i];
+	}
+}
+
+void printRowsSum(int arr[3], int arrayLength, int rowNumber)
+{
+	printf("Row %d Sum = %d\n",rowNumber, arraySum(arr, arrayLength));
+	cout << endl;
+}
+
+int main() {
+
+	srand((unsigned)time(NULL));
+
+	int array[3][3];
+
+	fillMatrixWithRandomNumbers(array, 3, 3);
+
+	cout << "The following is 3 x 3 Random Matrix:\n";
+	printMatrix(array, 3, 3);
+
+	cout << "The following is sum of each row in matrix:\n";
+	
+	int row1[3];
+	int row2[3];
+	int row3[3];
+
+k	saveRowInArray(array, 1, 3, row1);
+	saveRowInArray(array, 2, 3, row2);
+	saveRowInArray(array, 3, 3, row3);
+
+	printRowsSum(row1, 3, 1);
+	printRowsSum(row2, 3, 2);
+	printRowsSum(row3, 3, 3);
+
+	return 0;
+
+
+
+}
