260d4b7980782373c672543a7b83995075445fe7
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-02-01 07:47:10 +0300

    32/3#: Homework is Vowel?

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -5,71 +5,29 @@
 
 using namespace std;
 
-enum enCase {Upper =0, Lower = 1, NoCase = 3};
-
-enCase findCaseType(char letter)
+bool isVowel(char letter)
 {
-	return (isupper(letter) ? enCase::Upper : enCase::Lower);
-}
+	char vowelLetters[5] = {'a', 'e', 'i', 'o', 'u'};
 
-short countLetterInString(string str, char letter)
-{
-	short count = 0;
-	for (short i = 0; i < str.length(); i++)
+	for (char& vowelLetter : vowelLetters)
 	{
-		if (str[i] == letter)
-			count++;
+		if (vowelLetter == letter)
+			return true;
 	}
-
-	return count;
-}
-
-short countLetterInStringNoCase(string str, char letter)
-{
-	short count = 0;
-	for (short i = 0; i < str.length(); i++)
-	{
-		if (str[i] == letter || str[i] == MyString::invertLetterCase(letter))
-			count++;
-	}
-
-	return count;
-}
-
-short countLettersInString(string str, char letter, enCase caseType = enCase::Lower)
-{
-	short count = 0;
-
-	if (caseType != enCase::NoCase)
-	{
-		caseType = findCaseType(letter);
-	}
-
-	switch (caseType)
-	{
-	case Upper:
-	case Lower:
-		count = countLetterInString(str, letter);
-		break;
-
-	case NoCase:
-		count = countLetterInStringNoCase(str, letter);
-		break;
-	}
-	
-
-	return count;
+	return false;
 }
 
 int main() {
 	
 
-	string sentence = MyRead::readString("Please enter your String:\n");
+	
 	char letter = MyRead::readChar("Please enter a character:\n");
 
 	
-	cout << "\nLetter " << letter << " Count = " << countLettersInString(sentence, letter) << endl;
-	cout << "\nLetter " << letter << " Or " << MyString::invertLetterCase(letter) << " Count = " << countLettersInString(sentence, letter, enCase::NoCase) << endl;
+	if (isVowel(letter))
+		cout << "\nYES Letter '" << letter << "' is vowel" << endl;
+	else
+		cout << "\nNO Letter '" << letter << "' is not vowel" << endl;
 
 	return 0;
 
