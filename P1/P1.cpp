66c93e31266075cfdc556c81abe9451753415b5e
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-02-12 12:17:13 +0300

    39/3#: Homework Join String

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -6,50 +6,26 @@
 
 using namespace std;
 
-string trimLeft(string sentence)
+string joinString(vector<string> vWords, string delimiter)
 {
-	
-	for (short i = 0; i < sentence.length(); i++)
+	string sentence = vWords.at(0);
+
+	for (int i = 1; i < vWords.size(); i++)
 	{
-		if (sentence[i] != ' ')
-		{
-			return sentence.substr(i, sentence.length() - i);
-		}
-
+		sentence += delimiter + vWords.at(i);
 	}
-	return "";
-}
 
-string trimRight(string sentence)
-{
-
-	for (short i = sentence.length(); i >= 0; i--)
-	{
-		if (sentence[i] != ' ')
-		{
-			return sentence.substr(0, i + 1);
-		}
-
-	}
-	return "";
-}
-
-string trim(string sentence)
-{
-	return(trimLeft(trimRight(sentence)));
+	return sentence;
 }
 
 int main() {
 
+	vector <string> vWords{ "Ahmad", "Ali", "Ghala", "Sara" };
+	cout << "\nVector after join:\n";
+	cout << joinString(vWords, "|") << endl;
 
 
-	string sentence = MyRead::readString("Please enter a String:\n");
 
-
-	cout << "String = " << sentence << endl;
-	cout << "Trim Left = " << trimLeft(sentence) << endl;
-	cout << "Trim Right = " << trimRight(sentence) << endl;
-	cout << "Trim = " << trim(sentence) << endl;
 
 	return 0;
 
