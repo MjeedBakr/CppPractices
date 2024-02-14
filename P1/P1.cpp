1ad37d9fe64b0f949206e7106e9db5e82ced8f45
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-02-14 07:03:59 +0300

    43/3#: Homework replace words case sensitive

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -6,27 +6,58 @@
 
 using namespace std;
 
-string replaceWordInString(string sentence, string oldWord, string newWord)
+string convertToUpper(string str)
 {
-	short position = sentence.find(oldWord);
 
-	while (position != std::string::npos)
+	for (char& i : str)
+		i = toupper(i);
+
+	return str;
+}
+
+string replaceWordInStringnoMatch(string sentence, string oldWord, string newWord)
+{
+	string s1 = "";
+	int position = 0;
+
+	string upperSentence = convertToUpper(sentence);
+	string upperOldWord = convertToUpper(oldWord);
+
+	while ((position = upperSentence.find(upperOldWord)) != std::string::npos)
 	{
-		sentence = sentence.replace(position, oldWord.length(), newWord);
-		position = sentence.find(oldWord);
+		s1 += sentence.substr(0, position) + newWord;
+		sentence.erase(0, position + oldWord.length());
+		upperSentence.erase(0, position + oldWord.length());
 	}
 
-	return sentence;
+	return s1 + sentence;
+}
+
+string replaceWordInString(string sentence, string oldWord, string newWord)
+{
+	string s1 = "";
+	int position = 0;
+
+	while ((position = sentence.find(oldWord)) != std::string::npos)
+	{
+		s1 += sentence.substr(0, position) + newWord;
+		sentence.erase(0, position + oldWord.length());
+	}
+
+	return s1 + sentence;
 }
 
 
 int main() {
 
-	string sentence = MyRead::readString("Please Enter Your String:\n");
+	string sentence = "Welcome to Jordan , Jordan is a nice country";
 
 
-	cout << "\nString after replace:";
-	cout << "\n" << replaceWordInString(sentence, "Jordan", "USA");
+	cout << "\nString after replace match case:";
+	cout << "\n" << replaceWordInString(sentence, "jordan", "Saudi Arabia");
+
+	cout << "\nString after replace no match case:";
+	cout << "\n" << replaceWordInStringnoMatch(sentence, "jordan", "Saudi Arabia");
 	
 
 	system("pause>0");
