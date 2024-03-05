efc891a628efd7f22ad40b46ba5d80dbe86fbf4a
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-03-05 17:47:09 +0300

    3/4#: Homework Leap Year (one line of code)

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -7,16 +7,7 @@ using namespace std;
 
 bool isLeapYear(short year)
 {
-	if (year % 400 == 0)
-	{
-		return true;
-	}
-	else if (year % 4 == 0 && year % 100 != 0)
-	{
-		return true;
-	}
-
-	return false;
+	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 }
 
 int main() {
