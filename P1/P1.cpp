06bfb9ec708e4d8636a31ac796d21ee4ecbf9509
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-03-05 18:00:05 +0300

    4/4#: Homework Number of Days-Hours-Minutes-Seconds in a year

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -10,15 +10,37 @@ bool isLeapYear(short year)
 	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 }
 
+short getNumbersOfDays(short year)
+{
+	if (isLeapYear(year))
+		return 366;
+
+	return 365;
+}
+
+short getNumberOfHours(short year)
+{
+	return 24 * getNumbersOfDays(year);
+}
+
+long getNumberOfMinutes(short year)
+{
+	return 60 * getNumberOfHours(year);
+}
+
+long getNumberOfSeconds(short year)
+{
+	return 60 * getNumberOfMinutes(year);
+}
+
 int main() {
 
-	short number;
-	while (true)
-	{
-		cout << "Enter a year: ";
-		cin >> number;
-		cout << isLeapYear(number) << endl;
-	}
+	short year = 1971;
+	
+	cout << getNumbersOfDays(year) << endl;
+	cout << getNumberOfHours(year) << endl;
+	cout << getNumberOfMinutes(year) << endl;
+	cout << getNumberOfSeconds(year) << endl;
 	
 
 	system("pause>0");
