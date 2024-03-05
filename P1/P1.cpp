9565774e95d2d07a141438bb26dbb75f2eef007f
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-03-05 18:30:07 +0300

    5/4#: Homework Number of Days-Hours-Minutes-Seconds in a month

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -10,37 +10,75 @@ bool isLeapYear(short year)
 	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 }
 
-short getNumbersOfDays(short year)
+short getNumbersOfDaysInYear(short year)
 {
-	if (isLeapYear(year))
-		return 366;
-
-	return 365;
+	return isLeapYear(year) ? 366 : 365;
 }
 
-short getNumberOfHours(short year)
+short getNumberOfHoursInYear(short year)
 {
-	return 24 * getNumbersOfDays(year);
+	return 24 * getNumbersOfDaysInYear(year);
 }
 
-long getNumberOfMinutes(short year)
+long getNumberOfMinutesInYear(short year)
 {
-	return 60 * getNumberOfHours(year);
+	return 60 * getNumberOfHoursInYear(year);
 }
 
-long getNumberOfSeconds(short year)
+long getNumberOfSecondsInYear(short year)
 {
-	return 60 * getNumberOfMinutes(year);
+	return 60 * getNumberOfMinutesInYear(year);
 }
+
+short getNumberOfDaysInMonth(short year, short month)
+{
+	switch (month)
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		return 31;
+
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		return 30;
+
+	case 2:
+		return isLeapYear(year) ? 29 : 28;
+	}
+}
+
+short getNumberOfHoursInMonth(short year, short month)
+{
+	return 24 * getNumberOfDaysInMonth(year, month);
+}
+
+long getNumberOfMinutesInMonth(short year, short month)
+{
+	return 60 * getNumberOfHoursInMonth(year, month);
+}
+
+long getNumberOfSecondsInMonth(short year, short month)
+{
+	return 60 * getNumberOfMinuterInMonth(year, month);
+}
+
 
 int main() {
 
-	short year = 1971;
+	short year = 2000;
+	short month = 2;
 	
-	cout << getNumbersOfDays(year) << endl;
-	cout << getNumberOfHours(year) << endl;
-	cout << getNumberOfMinutes(year) << endl;
-	cout << getNumberOfSeconds(year) << endl;
+	cout << getNumberOfDaysInMonth(year, month) << endl;
+	cout << getNumberOfHoursInMonth(year, month) << endl;
+	cout << getNumberOfMinutesInMonth(year, month) << endl;
+	cout << getNumberOfSecondsInMonth(year, month) << endl;
 	
 
 	system("pause>0");
