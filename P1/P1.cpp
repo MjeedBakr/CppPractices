3725e9f43063f3ffe358dab3b50825f6621edb59
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-03-06 11:52:46 +0300

    6/4#: Homework Number of Days-Hours-Minutes-Seconds in a month Two line code

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -35,17 +35,8 @@ short getNumberOfDaysInMonth(short year, short month)
 	if (month < 1 || month > 12)
 		return 0;
 
-	if (month == 2)
-		return isLeapYear(year) ? 29 : 28;
-
-	short arr31Days[7] = {1, 3, 5, 7, 8, 10, 12};
-
-	for (short i = 0; i <= 7; i++)
-		if (arr31Days[i - 1] == month)
-			return 31;
-
-	//Then its 30 Days
-	return 30;
+	int numberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	return (month == 2) ? (isLeapYear(year) ? 29 : 28) : numberOfDays[month - 1];
 }
 
 short getNumberOfHoursInMonth(short year, short month)
@@ -60,14 +51,14 @@ long getNumberOfMinutesInMonth(short year, short month)
 
 long getNumberOfSecondsInMonth(short year, short month)
 {
-	return 60 * getNumberOfMinuterInMonth(year, month);
+	return 60 * getNumberOfMinutesInMonth(year, month);
 }
 
 
 int main() {
 
 	short year = 2000;
-	short month = 2;
+	short month = 12;
 	
 	cout << getNumberOfDaysInMonth(year, month) << endl;
 	cout << getNumberOfHoursInMonth(year, month) << endl;
