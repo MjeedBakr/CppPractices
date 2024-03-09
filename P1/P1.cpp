70cc9af76608c8e671d73061620307638d7b1c83
Abdulmajeed <s443011939@st.uqu.edu.sa>
2024-03-09 19:47:25 +0300

    7/4#: Homework Day Name

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -5,66 +5,47 @@
 
 using namespace std;
 
-bool isLeapYear(short year)
+string getDayName(short dayNumber)
 {
-	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+	string arrDays[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	
+	return arrDays[dayNumber];
 }
 
-short getNumbersOfDaysInYear(short year)
+string getDateFormat(short day, short month, short year)
 {
-	return isLeapYear(year) ? 366 : 365;
+	string date = to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+	return date;
 }
 
-short getNumberOfHoursInYear(short year)
+// Gregorian Calender
+short getDayOrder(short day, short month, short year)
 {
-	return 24 * getNumbersOfDaysInYear(year);
+	short a, y, m;
+	a = (14 - month) / 12;
+	y = year - a;
+	m = month + (12 * a) - 2;
+
+	return (day + y + (y/4) - (y/100) + (y/400) + ((31 * m) / 12) ) % 7;
 }
-
-long getNumberOfMinutesInYear(short year)
-{
-	return 60 * getNumberOfHoursInYear(year);
-}
-
-long getNumberOfSecondsInYear(short year)
-{
-	return 60 * getNumberOfMinutesInYear(year);
-}
-
-short getNumberOfDaysInMonth(short year, short month)
-{
-	if (month < 1 || month > 12)
-		return 0;
-
-	int numberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	return (month == 2) ? (isLeapYear(year) ? 29 : 28) : numberOfDays[month - 1];
-}
-
-short getNumberOfHoursInMonth(short year, short month)
-{
-	return 24 * getNumberOfDaysInMonth(year, month);
-}
-
-long getNumberOfMinutesInMonth(short year, short month)
-{
-	return 60 * getNumberOfHoursInMonth(year, month);
-}
-
-long getNumberOfSecondsInMonth(short year, short month)
-{
-	return 60 * getNumberOfMinutesInMonth(year, month);
-}
-
 
 int main() {
 
-	short year = 2000;
-	short month = 12;
-	
-	cout << getNumberOfDaysInMonth(year, month) << endl;
-	cout << getNumberOfHoursInMonth(year, month) << endl;
-	cout << getNumberOfMinutesInMonth(year, month) << endl;
-	cout << getNumberOfSecondsInMonth(year, month) << endl;
-	
+	short year;
+	cout << "Please enter a year: ";
+	cin >> year;
+
+	short month;
+	cout << "\nPlease enter a Month: ";
+	cin >> month;
+
+	short day;
+	cout << "\nPlease enter a Day: ";
+	cin >> day;
+
+	cout << "\nDate       :" << getDateFormat(day, month, year) << endl;
+	cout << "Day Order  :" << getDayOrder(day, month, year) << endl;
+	cout << "Day Name   :" << getDayName(getDayOrder(day, month, year));
 
 	system("pause>0");
 	return 0;
