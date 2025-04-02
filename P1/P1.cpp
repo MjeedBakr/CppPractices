b7896ed67f1e910d7fbff4a579759450f09e1acf
Abdulmajeed <mjeed.bakr@gmail.com>
2025-04-02 23:53:38 +0300

    Access Specifiers: Public, Private, Protected

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -7,32 +7,41 @@ using namespace std;
 
 class clsPerson
 {
-	int x;
-
 private:
-	int age;
-
-public:
-	string firstName;
-	string lastName;
-
-	string fullName()
+	//only accessable inside this calass
+	int Variabl1 = 5;
+	int Function1()
 	{
-		return firstName + " " + lastName;
+		return 40;
 	}
-
+protected:
+	//only accessable inside this calass and all classes inhirets this calss
+		int Variabl2 = 100;
+	int Function2()
+	{
+		return 50;
+	}
+public:
+	//accessable for everyone outside/inside/and classes inherits this class
+		string FirstName;
+	string LastName;
+	string FullName()
+	{
+		return FirstName + " " + LastName;
+	}
+	float Function3()
+	{
+		return Function1() * Variabl1 * Variabl2;
+	}
 };
 
-int main() {
 
-	clsPerson person1;
 
-	person1.firstName = "Abdulmajeed";
-	person1.lastName = "Bakr";
-
-	cout << person1.fullName() << endl;
-
-	system("pause>0");
-	return 0;
-
+int main()
+{
+	clsPerson Person1;
+	Person1.FirstName = "Mohammed";
+	Person1.LastName = "Abu-Hadhoud";
+	cout << "Person1: " << Person1.FullName() << endl;
+	cout << Person1.Function3();
 }
