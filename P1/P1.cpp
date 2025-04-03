c5bbad1c7079d4fbb56febbb3b3c03136fe72f01
Abdulmajeed <mjeed.bakr@gmail.com>
2025-04-03 18:30:38 +0300

    set & get

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -8,40 +8,44 @@ using namespace std;
 class clsPerson
 {
 private:
-	//only accessable inside this calass
-	int Variabl1 = 5;
-	int Function1()
-	{
-		return 40;
-	}
-protected:
-	//only accessable inside this calass and all classes inhirets this calss
-		int Variabl2 = 100;
-	int Function2()
-	{
-		return 50;
-	}
+	string _firstName;
+	string _lastName;
+
 public:
-	//accessable for everyone outside/inside/and classes inherits this class
-		string FirstName;
-	string LastName;
-	string FullName()
+	void setFirstName(string firstName)
 	{
-		return FirstName + " " + LastName;
+		_firstName = firstName;
 	}
-	float Function3()
+
+	string firstName()
 	{
-		return Function1() * Variabl1 * Variabl2;
+		return _firstName;
 	}
+
+	void setLastName(string lastName)
+	{
+		_lastName = lastName;
+	}
+
+	string lastName()
+	{
+		return _lastName;
+	}
+
+	string fullName()
+	{
+		return _firstName + " " + _lastName;
+	}
+
 };
 
 
 
 int main()
 {
-	clsPerson Person1;
-	Person1.FirstName = "Mohammed";
-	Person1.LastName = "Abu-Hadhoud";
-	cout << "Person1: " << Person1.FullName() << endl;
-	cout << Person1.Function3();
+	clsPerson person1;
+	person1.setFirstName("Abdulmajeed");
+	person1.setLastName("Bakr");
+
+	cout << "Person1: " << person1.fullName() << endl;
 }
