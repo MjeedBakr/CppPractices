54fb1f53263bc30f0180cf00782033798e417ab2
Abdulmajeed <mjeed.bakr@gmail.com>
2025-05-01 17:17:32 +0300

    Person exercise

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -5,28 +5,129 @@
 
 using namespace std;
 
-class clsA
-{
+
+class clsPerson {
+
+private:
+	short _id;
+	string _firstName;
+	string _lastName;
+	string _email;
+	string _phoneNumber;
+	static int counter;
+
+	short getID()
+	{
+		return _id;
+	}
+
+	void setFirstName(string firstName)
+	{
+		_firstName = firstName;
+	}
+
+	string getFirstName()
+	{
+		return _firstName;
+	}
+
+	void setLastName(string lastName)
+	{
+		_lastName = lastName;
+	}
+
+	string getlastName()
+	{
+		return _lastName;
+	}
+
+	void setEmail(string email)
+	{
+		_email = email;
+	}
+
+	string getEmail()
+	{
+		return _email;
+	}
+
+	void setPhoneNumber(string phoneNumber)
+	{
+		_phoneNumber = phoneNumber;
+	}
+
+	string getPhoneNumber()
+	{
+		return _phoneNumber;
+	}
+
+	string getFullName()
+	{
+		return _firstName + " " + _lastName;
+	}
+
 public:
-	static int Function1()
+	clsPerson(string firstName, string lastName, string email, string phoneNumber)
 	{
-		return 10;
+		_firstName = firstName;
+		_lastName = lastName;
+		_email = email;
+		_phoneNumber = phoneNumber;
+		_id = counter;
+		counter++;
 	}
-	int Function2()
+
+	__declspec(property(get = getID)) short id;
+	__declspec(property(get = getFirstName, put = setFirstName)) string firstName;
+	__declspec(property(get = getLastName, put = setLastName)) string lastName;
+	__declspec(property(get = getFullName)) string fullName;
+	__declspec(property(get = getEmail, put = setEmail)) string email;
+	__declspec(property(get = getPhoneNumber, put = setPhoneNumber)) string phoneNumber;
+
+	void print()
 	{
-		return 20;
+		cout << "Info:\n";
+		cout << "_______________________________________\n";
+		cout << "ID               :" << _id << "\n";
+		cout << "First Name       :" << _firstName << "\n";
+		cout << "Last Name        :" << _lastName << "\n";
+		cout << "Full Name        :" << fullName << "\n";
+		cout << "Email            :" << _email << "\n";
+		cout << "Phone            :" << _phoneNumber << "\n";
+		cout << "_______________________________________\n";
+
+		cout << endl;
 	}
+
+	void sendEmail(string subject, string content)
+	{
+		cout << "The Following Message sent successfully to email: " << _email << "\n";
+		cout << "Subject: " << subject << "\n";
+		cout << "Body: " << content << "\n";
+
+		cout << endl;
+	}
+
+	void sendSMS(string content)
+	{
+		cout << "The Following Message sent successfully to phone: " << _phoneNumber << "\n";
+		cout << content << "\n";
+
+		cout << endl;
+	}
+
 };
+
+int clsPerson::counter = 0;
 
 int main()
 {
-	//The following line calls static function directly via class not through the object
-	//At class level you can call only static methods and static members
-	cout << clsA::Function1() << endl;
+	clsPerson person1("Adbulmajeed", "Bakr", "mjeed.bakr@gmail.com", "0544362329");
+	person1.print();
 
-	//static methods can also be called throught the object.
-	clsA A1, A2;
-	cout << A1.Function1() << endl;
-	cout << A1.Function2() << endl;
-	cout << A2.Function1() << endl;
+	person1.sendEmail("Hi", "How are you?");
+	person1.sendSMS("How are you?");
+
+	system("pause>0");
+	return 0;
 }
